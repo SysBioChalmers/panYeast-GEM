0b51f9d9e732171e2f3b778e16f17565e7cd2671
FEIRANL <feiranl@chalmers.se>
2019-02-19 11:27:52 +0100

    feat-rxns:add pan new rxns into the model

--- ComplementaryScripts/addPanModelRxn.m
+++ ComplementaryScripts/addPanModelRxn.m
@@ -92,5 +92,79 @@ for i = 1:length(newmet.metNames)
     end
 end
 
+%Load rxnProp(rev and GPR)
+fid = fopen('../ComplementaryData/PanNewRxnProp.tsv');
+rev = textscan(fid,'%s %s %s %s %s %s %s %s %s %s','Delimiter','\t','HeaderLines',1);
+newrxn.ID  = rev{1};
+newrxn.Rev = cellfun(@str2num, rev{2});
+newrxn.GPR = rev{3};
+newrxn.rxnNames     = rev{4};
+newrxn.rxnECNumbers = rev{5};
+newrxn.subSystems   = rev{6};
+newrxn.rxnKEGGID    = rev{7};
+newrxn.rxnMetaNetXID   = rev{8};
+newrxn.rxnNotes    = rev{9};
+fclose(fid);
+
+%add new reactions according to rev ID. Met Coef need to be in the column,
+%not a row. Coef should be double, which was converted at the import
+%section.
+EnergyResults     = {};
+MassChargeresults = {};
+RedoxResults      = {};
+cd otherchanges/
+for i = 1:length(newrxn.ID)
+    newID   = getNewIndex(model.rxns);
+    
+    j = find(strcmp(matrix.rxnIDs,newrxn.ID{i}));
+    Met = matrix.mets(j);
+    Coef = transpose(matrix.metcoef(j));
+    model = addReaction(model,...
+                        ['r_' newID],...
+                        'reactionName', newrxn.ID{i},...
+                        'metaboliteList',Met,...
+                        'stoichCoeffList',Coef,...
+                        'reversible',newrxn.Rev(i,1),...
+                        'geneRule',newrxn.GPR{i},...
+                        'checkDuplicate',1);
+    [EnergyResults,RedoxResults] = CheckEnergyProduction(model,{['r_' newID]},EnergyResults,RedoxResults);
+    [MassChargeresults] = CheckBalanceforSce(model,{['r_' newID]},MassChargeresults);
+end
+cd ..
+
+% add gene standard name for new genes
+fid = fopen('../ComplementaryData/databases/SGDgeneNames.tsv');
+yeast_gene_annotation = textscan(fid,'%s %s','Delimiter','\t','HeaderLines',1);
+fclose(fid);
+
+geneIndex = zeros(1,1);
+for i = 1: length(model.genes)
+    geneIndex = strcmp(yeast_gene_annotation{1}, model.genes{i});
+    if sum(geneIndex) == 1 && ~isempty(yeast_gene_annotation{2}{geneIndex})
+        model.geneNames{i} = yeast_gene_annotation{2}{geneIndex};
+    else
+        model.geneNames{i} = model.genes{i};
+    end
+end
+
+% Add protein name for genes
+for i = 1:length(model.genes)
+    model.proteins{i} = strcat('COBRAProtein',num2str(i));
+end
+
+%add rxn annotation
+for i = 1:length(newrxn.ID)
+    [~,rxnID] = ismember(newrxn.ID(i),model.rxnNames);
+    if rxnID ~= 0
+        model.rxnNames{rxnID}     = newrxn.rxnNames{i};
+        model.rxnECNumbers(rxnID) = newrxn.rxnECNumbers(i);
+        model.rxnKEGGID(rxnID)    =  newrxn.rxnKEGGID(i);
+        model.rxnMetaNetXID(rxnID)    =  newrxn.rxnMetaNetXID(i);
+        %model.subSystems(rxnID)    =  newrxn.subSystems(i);
+        model.rxnNotes(rxnID)    =  newrxn.rxnNotes(i);
+    end
+end
+
 % Save model:
+model = rmfield(model,'grRules');
 saveYeastModel(model)
