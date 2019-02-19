9b89d165bec60e67910e30bcee5108916258bcb1
FEIRANL <feiranl@chalmers.se>
2019-02-19 11:41:09 +0100

    feat-grRules:change genes only in GEM to panIDs
    genesonlyinGEM.tsv is the file that stores rxns involved. genesonlyinGEMPanIDMapping.tsv is the file that stores all genes that only in GEM

---
+++ ComplementaryScripts/changeGenesonlyinGEMtoPanID.m
@@ -0,0 +1,72 @@
+function model = changeGenesonlyinGEMtoPanID
+%This functio is to change genes only exist in GEM to PanID
+%input: a mapping list of genes; a cobra model
+%output: new model
+
+
+
+
+
+model = loadYeastModel;
+modelr = ravenCobraWrapper(model);
+%Load mapping list:
+fid = fopen('../ComplementaryData/genesonlyinGEM.tsv');
+mapplist = textscan(fid,'%s %s %s %s %s','Delimiter','\t','HeaderLines',1);
+mapping.rxnIDs  = mapplist{3};
+mapping.new_GPR  = mapplist{5};
+fclose(fid);
+
+
+
+[~,rxnindex] = ismember(mapping.rxnIDs,model.rxns);
+for i = 1:length(rxnindex)
+    model = changeGeneAssociation(model, model.rxns{rxnindex(i)},mapping.new_GPR{i});
+end
+        
+
+
+
+% Add gene standard name for new genes
+fid = fopen('../ComplementaryData/databases/SGDgeneNames.tsv');
+yeast_gene_annotation = textscan(fid,'%s %s','Delimiter','\t','HeaderLines',1);
+fclose(fid);
+for i = 1: length(model.genes)
+    geneIndex = strcmp(yeast_gene_annotation{1}, model.genes{i});
+    if sum(geneIndex) == 1 && ~isempty(yeast_gene_annotation{2}{geneIndex})
+        model.geneNames{i} = yeast_gene_annotation{2}{geneIndex};
+    else
+        model.geneNames{i} = model.genes{i};
+    end
+end
+
+% Add protein name for genes
+for i = 1:length(model.genes)
+    model.proteins{i} = strcat('COBRAProtein',num2str(i));
+end
+
+
+% Save model:
+model = removeUnusedGenes(model);
+saveYeastModel(model)
+
+
+
+
+[~,geneindex] = ismember(mapping.IDs,model.genes);
+geneSets1 = [];
+rxnID1 = [];
+for i = 1:length(geneindex)
+    if geneindex(i) ~= 0
+    rxnID = find(model.rxnGeneMat(:,geneindex(i)));
+    rxnID1 = [rxnID1;rxnID]
+    geneSets1 = [geneSets1;modelr.grRules(rxnID)];
+    end
+end
+for j = 1:length(rxnID)
+    model = changeGeneAssociation(model, model.rxns{rxnID(j)}, ['( ',geneSets1{j}, ' )']);
+end
+if isfield(model,'grRules')
+    model = rmfield(model,'grRules');
+end
+modelr = ravenCobraWrapper(model);
+end
